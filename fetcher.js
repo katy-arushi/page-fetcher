20944cef558f1ebd5da6081ac7587d4773f21ea0
Arushi Katyal <katy.arushi@gmail.com>
2021-08-23 14:31:53 -0400

    remove unnecessary variable for body

--- fetcher.js
+++ fetcher.js
@@ -5,14 +5,12 @@ const pageURL = process.argv[2];
 const filePath = process.argv[3];
 
 request(pageURL, (error, response, body) => {
-  let bodyData = body;
-
   if ((response !== undefined) && (response.statusCode === 200)) {
-    fs.appendFile(filePath, bodyData, function(err) {
+    fs.appendFile(filePath, body, function(err) {
       if (err) {
         throw err;
       }
-      console.log(`Downloaded and saved ${bodyData.length} bytes to ${filePath}`);
+      console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`);
     });
   } else {
     console.log('Sorry, an error occured and the program must quit.');
