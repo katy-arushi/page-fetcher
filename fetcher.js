b21ecc3dd27217e0cb79e85e93facc31d73f1f83
Arushi Katyal <katy.arushi@gmail.com>
2021-08-23 00:33:14 -0400

    linting error fix

--- fetcher.js
+++ fetcher.js
@@ -1,22 +1,21 @@
-const fs = require('fs')
+const fs = require('fs');
 const request = require('request');
 
-const pageURL = process.argv[2]
-const filePath = process.argv[3]
+const pageURL = process.argv[2];
+const filePath = process.argv[3];
 
 request(pageURL, (error, response, body) => {
   let bodyData = body;
 
   if ((response !== undefined) && (response.statusCode === 200)) {
-    fs.appendFile(filePath, bodyData, function (err, data) {
+    fs.appendFile(filePath, bodyData, function(err) {
       if (err) {
         throw err;
-      }  
+      }
       console.log(`Downloaded and saved ${bodyData.length} bytes to ${filePath}`);
     });
   } else {
-    console.log('Sorry, an error occured and the program must quit.')
+    console.log('Sorry, an error occured and the program must quit.');
     process.exit();
   }
-
 });
